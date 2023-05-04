ca591dcb72aaf93a55018cb8fb1e646fc4e4aeac
Sarah Lewis <sarahlewis1808@gmail.com>
2023-05-04 14:00:36 +1200

    add project links

--- client/components/Projects.tsx
+++ client/components/Projects.tsx
@@ -5,37 +5,40 @@ const Projects = () => {
     <>
       <section id="projects">
         <div className="project">
+          <h3>SIMS Scribble98</h3>
+          <p>
+            Collaborative drawing game co-developed as part of Dev Academy final
+            project
+          </p>
           <a
             href="https://github.com/sarah-lewis-1808/sims-final-project/tree/sarah-lewis-1808-patch-1"
             target="_blank"
             rel="noreferrer"
           >
-            <h3>SIMS Scribble98</h3>
+            <button>Check it out</button>
           </a>
-          <p>
-            Collaborative drawing game co-developed as part of Dev Academy final
-            project
-          </p>
         </div>
         <div className="project">
+          <h3>PORTFOLIO</h3>
+          <p>Check out the plans for this portfolio</p>
           <a
             href="https://www.figma.com/file/1A1LD0UUwydAiu5fTh33HT/Untitled?node-id=2%3A597&t=OxdMVuiifidvObmG-1"
             target="_blank"
             rel="noreferrer"
           >
-            <h3>PORTFOLIO</h3>
+            <button>Check it out</button>
           </a>
-          <p>Check out the plans for this portfolio</p>
         </div>
         <div className="project">
+          <h3>TODO FULL STACK</h3>
+          <p>My first full stack project deployment</p>
           <a
             href="https://sarah-todo-fullstack.devacademy.online/"
             target="_blank"
             rel="noreferrer"
           >
-            <h3>TODO FULL STACK</h3>
+            <button>Check it out</button>
           </a>
-          <p>My first full stack project deployment</p>
         </div>
       </section>
     </>
