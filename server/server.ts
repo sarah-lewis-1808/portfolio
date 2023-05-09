6bbd3006ec1d835a94026dfa633d7d6d6062e485
Sarah Lewis <sarahlewis1808@gmail.com>
2023-05-09 15:01:35 +1200

    remove unused routes

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,10 @@
 import express from 'express'
 import { join } from 'node:path'
 
-import projectRoutes from './routes/projects'
-
 const server = express()
 
 server.use(express.json())
 server.use(express.static(join(__dirname, 'public')))
-
-server.use('/api/v1/projects', projectRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(join(__dirname, 'public/index.html'))
