cf8c5f069c141d63baed7af3116ea9a87b52d119
Sarah Lewis <sarahlewis1808@gmail.com>
2023-05-09 13:51:30 +1200

    fix broken link

--- server/public/data.ts
+++ server/public/data.ts
@@ -6,7 +6,7 @@ const projects: Project[] = [
     intro: 'Collaborative multi player drawing game',
     tech: 'Typescript | TailwindCSS | React',
     link: 'https://sims-scribble98.netlify.app/',
-    image: 'server/public/images/Screenshot 2023-05-09 at 1.26.24 PM.png',
+    image: '/images/SIMS-Scribble98.png',
     reflection: [],
   },
   {
@@ -14,15 +14,15 @@ const projects: Project[] = [
     intro: 'Creative portfolio to display my skills and interests',
     tech: 'Typescript | CSS | React',
     link: '#',
-    image: 'server/public/images/layer-12.svg',
+    image: '/images/layer-12.svg',
     reflection: [],
   },
   {
     name: 'ToDo List',
     intro: 'Deployed full stack web app with simple todo list functionality',
     tech: 'Typescript | SCSS | React',
-    link: 'server/public/images/layer-12.svg',
-    image: 'server/public/images/todo-fullstack.png',
+    link: 'https://sarah-todo-fullstack.devacademy.online/',
+    image: '/images/todo-fullstack.png',
     reflection: [],
   },
 ]
