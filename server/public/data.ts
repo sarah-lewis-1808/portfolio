76633b67060a2e251c2a65b67370df6f40a16d81
Sarah Lewis <sarahlewis1808@gmail.com>
2023-05-09 14:57:32 +1200

    fix up public folder

--- server/public/data.ts
+++ server/public/data.ts
@@ -7,7 +7,11 @@ const projects: Project[] = [
     tech: 'Typescript | TailwindCSS | React',
     link: 'https://sims-scribble98.netlify.app/',
     image: '/images/SIMS-Scribble98.png',
-    reflection: [],
+    reflection: [
+      'As part of our final project for Dev Academy we co-developed a game based on the concept of `telephone` as a team of 4. The game begins with player 1 providing an initial caption for player 2 to draw, then the next player describes that drawing - and so on. The end result is a number of captions and illustrations that progressively become more unhinged with the more players involved!',
+      'The main technologies we used were Typescript, React, TailwindCSS. Data is stored in an SQLite database and the user drawn images were captured and converted to svg files to reference later.',
+      '',
+    ],
   },
   {
     name: 'Portfolio',
@@ -15,7 +19,9 @@ const projects: Project[] = [
     tech: 'Typescript | CSS | React',
     link: '#',
     image: 'server/public/images/portfolio.png',
-    reflection: [],
+    reflection: [
+      'I`ve been so excited about knowing so many new skills that I`ve been having trouble deciding what to use to accurately showcase my abilities! ',
+    ],
   },
   {
     name: 'ToDo List',
@@ -23,7 +29,11 @@ const projects: Project[] = [
     tech: 'Typescript | SCSS | React',
     link: 'https://sarah-todo-fullstack.devacademy.online/',
     image: '/images/todo-fullstack.png',
-    reflection: [],
+    reflection: [
+      'This project was part of our assessment for Dev Academy where we were instructed to deploy a full stack web application using the skills we had learned over the course of 15 weeks.',
+      'The main technologies used is Typescript, HTML, React, Redux',
+      'I really enjoyed creating this app as the template for the styling was provided and our aim was to create the functionality. This allowed me to understand the full stack in more detail as I was responsible for creating the connections between the front and back end of the app and also the deployment.',
+    ],
   },
 ]
 
