bbf98bc2436505c7ed9f73532885c52847bedb4a
Sarah Lewis <sarahlewis1808@gmail.com>
2023-05-09 19:17:30 +1200

    improve responsiveness

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -2,14 +2,18 @@ const About = () => {
   return (
     <>
       <section id="about">
-        <div className="about">
-          <img src="/images/catface.jpg" alt="sarah with a cat" />
-        </div>
-        <div className="about-me-text">
-          <h3>{`Kia ora, I'm Sarah`}</h3>
-          <p>{`Creative thinker, thoughtful problem solver, avid planner and perpetual learner.`}</p>
-          <p>{`Looking for a software development role that will allow me to expand on my full stack experiences and showcase my communication and teamwork skills.`}</p>
-          <p>{`Currently based in Tāmaki Makaurau, Auckland`}</p>
+        <div className="wrapper">
+          <div id="about-left">
+            <div id="profile-pic">
+              <img src="/images/catface.jpg" alt="sarah with a cat" />{' '}
+            </div>
+          </div>
+          <div id="bio">
+            <h3>{`Kia ora, I'm Sarah`}</h3>
+            <p>{`Creative thinker, thoughtful problem solver, avid planner and perpetual learner.`}</p>
+            <p>{`Looking for a software development role that will allow me to expand on my full stack experiences and showcase my communication and teamwork skills.`}</p>
+            <p>{`Currently based in Tāmaki Makaurau, Auckland`}</p>
+          </div>
         </div>
       </section>
     </>
--- client/components/Projects.tsx
+++ client/components/Projects.tsx
@@ -3,16 +3,18 @@ import projects from '../../server/public/data'
 const Projects = () => {
   return (
     <section id="projects">
-      {projects.map((project, index) => (
-        <div key={index} className="project">
-          <h3 key={project.name}>{project.name}</h3>
-          <p key={project.intro}>{project.intro}</p>
-          <a key={project.link} href={project.link}>
-            <button key={`${project.name} button`}>Check it out</button>
-          </a>
-          <p key={project.tech}>{project.tech}</p>
-        </div>
-      ))}
+      <div className="wrapper">
+        {projects.map((project, index) => (
+          <div key={index} className="project">
+            <h3 key={project.name}>{project.name}</h3>
+            <p key={project.intro}>{project.intro}</p>
+            <p key={project.tech}></p>
+            <a key={project.link} href={project.link}>
+              <button key={`${project.name} button`}>Check it out</button>
+            </a>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
